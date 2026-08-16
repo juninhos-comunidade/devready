2120df9c85820529719181355776a1ee67e112c6
Isabela Duarte <isabela.duarte@zcksoftware.com.br>
2026-08-16 11:49:04 -0300

    docs: prepara documentação para entrega final

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,11 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    ".npm-cache/**",
     "out/**",
     "build/**",
+    "node_modules/**",
+    "app/generated/prisma/**",
     "next-env.d.ts",
   ]),
 ]);
